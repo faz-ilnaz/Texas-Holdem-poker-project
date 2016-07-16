c392a105e758d5091846910bae5103fb86cde7d5
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2016-07-16 15:05:04 +0300

    Pair programming with Ivan. Added Value enum, fields to player

--- src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
@@ -1,4 +1,33 @@
 package ru.innopolis.university.summerbootcamp2016.poker;
 
+import java.util.List;
+
 public class Player {
+
+    private String name;
+    private List<Card> cards;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
+
+    public void setCards(List<Card> cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "name='" + name + '\'' +
+                ", cards=" + cards +
+                '}';
+    }
 }
