37c55e4924f0f4fc4e52f1f001bb8099fcb05ff0
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2016-07-16 14:31:22 +0300

    project init

---
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Deck.java
@@ -0,0 +1,4 @@
+package ru.innopolis.university.summerbootcamp2016.poker;
+
+public class Deck {
+}
---
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Suit.java
@@ -0,0 +1,8 @@
+package ru.innopolis.university.summerbootcamp2016.poker;
+
+public enum Suit {
+    SPADES,
+    HEARTS,
+    DIAMONDS,
+    CLUBS
+}
