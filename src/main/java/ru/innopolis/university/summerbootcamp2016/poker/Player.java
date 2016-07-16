8440760448e7987b4ba5c9055eb08bfc5f13d952
addi1993 <sych-off@yandex.ru>
2016-07-17 00:25:39 +0300

    call function added

--- src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
@@ -7,7 +7,7 @@ public class Player {
     private String name;
     private long stake;
     private long balance;
-    private long id;
+    private long id;//starts from 2, 1 is a Table
     private List<Card> cards;
 
     public String getName() {
@@ -47,9 +47,33 @@ public class Player {
         }
         this.stake = stake;
         this.balance-=stake;
+        if(this.stake>Game.maxStake){
+            Game.maxStake = this.stake;
+        }
         return true;
     }
 
+    public void call(){
+        long diff = maxStake - stake;
+        if(setPlayerStake(diff)){
+            System.out.println("Call is correct");
+        }
+        else{
+            System.out.println("You can not call");
+        }
+    }
+
+    public void raise(){
+
+    }
+
+    public void check(){
+
+    }
+
+    public void fold(){
+
+    }
     @Override
     public String toString() {
         return "Player{" +
