4d89de7e1f0cd6de4c1402d11e3387c94389a86d
M <1>
2016-07-17 10:58:19 +0300

    Merge branch 'master' of https://github.com/faz-ilnaz/Texas-Holdem-poker-project into Maksim
    # Conflicts:
    #	src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Game.java

--- src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Game.java
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Game.java
@@ -54,8 +54,6 @@ public class Game {
             }
         }
 
-
-
         return 1;
     }
 }
--- src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
+++ src/main/java/ru/innopolis/university/summerbootcamp2016/poker/Player.java
@@ -1,5 +1,6 @@
 package ru.innopolis.university.summerbootcamp2016.poker;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
@@ -7,8 +8,8 @@ public class Player {
     private String name;
     private long stake;
     private long balance;
-    private long id;//starts from 2, 1 is a Table
-    private boolean smallBlind=false;
+    private int id;//starts from 2, 1 is a Table
+    private boolean smallBlind = false;
     private List<Card> cards;
 
     public String getName() {
@@ -36,52 +37,60 @@ public class Player {
     }
 
     //Method returns stake and resets it
-    public long collectPlayerStake(){
+    public long collectPlayerStake() {
         long s = stake;
-        stake=0;
+        stake = 0;
         return s;
     }
+
     //Function set stake and subtract balance if stake < balance
-    public boolean setPlayerStake(long stake){
-        if (stake > this.balance){
+    public boolean setPlayerStake(long stake) {
+        if (stake > this.balance) {
             return false;
         }
         this.stake = stake;
-        this.balance-=stake;
-        if(this.stake>Game.maxStake){
+        this.balance -= stake;
+        if (this.stake > Game.maxStake) {
             Game.maxStake = this.stake;
         }
         return true;
     }
 
-    public void call(){
-        long diff = Game.maxStake - stake;
-        if(setPlayerStake(diff)){
-            System.out.println("Call is correct");
-        }
-        else{
-            System.out.println("You can not call");
-        }
-    }
 
-    public boolean isSmallBlind(){
-        if(smallBlind)
+    public boolean isSmallBlind() {
+        if (smallBlind)
             return true;
         else
             return false;
     }
 
-    public void raise(){
+    public void takeCards(Deck deck) {
+        cards = new ArrayList<Card>();
+        cards.add(deck.getCard(id));
+        cards.add(deck.getCard(id));
+    }
+
+    public void call() {
+        long diff = Game.maxStake - stake;
+        if (setPlayerStake(diff)) {
+            System.out.println("Call is correct");
+        } else {
+            System.out.println("You can not call");
+        }
+    }
+
+    public void raise(long raiseVal) {
 
     }
 
-    public void check(){
+    public void check() {
 
     }
 
-    public void fold(){
+    public void fold() {
 
     }
+
     @Override
     public String toString() {
         return "Player{" +
